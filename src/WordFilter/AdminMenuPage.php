4f6d68fa8183b413f9abe607ebef36e024cbf284
hasankarim18 <hallysust@gmail.com>
2026-03-18 15:02:42 +0600

    form submit

--- src/WordFilter/AdminMenuPage.php
+++ src/WordFilter/AdminMenuPage.php
@@ -6,15 +6,53 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+
+
 class AdminMenuPage
 {
+
+    public function handleForm()
+    {
+        //AND current_user_can('manage_options')
+        if (isset($_POST['ourNonce'])) {
+            if (wp_verify_nonce($_POST['ourNonce'], 'saveFilterWords')):
+                update_option('plugin_words_to_filter', sanitize_text_field($_POST['plugin_words_to_filter']));
+                ?>
+                <div class="updated">
+                    <p>Your filtered word were saved.</p>
+                </div>
+
+                <?php
+            else:
+                ?>
+                <a href="<?php echo admin_url('admin.php?page=trovia-wp-word-filter'); ?>">Back to Word filter</a>
+                <?php
+                wp_die('Invalid nonce');
+            endif;
+        } else {
+            ?>
+            <a href="<?php echo admin_url('admin.php?page=trovia-wp-word-filter'); ?>">Back to Word filter</a>
+            <?php
+            wp_die('Nonce not set');
+
+        }
+
+
+    }
+
     public function word_filter_html()
     {
+        if (current_user_can('manage_options')) {
+            if (isset($_POST['justsubmitted']) && isset($_POST['submit']) && $_POST['justsubmitted'] == 'true') {
+                $this->handleForm();
+            }
+        } else {
+            wp_die('Form not submitted due to security reason.');
+        }
         $file = __DIR__ . '/views/main-page.php';
-
         if (file_exists($file)) {
             include_once($file);
-            ;
+
         }
 
     }
--- src/WordFilter/views/main-page.php
+++ src/WordFilter/views/main-page.php
@@ -1,11 +1,20 @@
 <div class="wrap">
     <h1>Word Filter</h1>
+    <?php
+    echo admin_url('admin.php?page=trovia-wp-word-filter');
+
+    ?>
     <form method="POST">
+        <input type="hidden" name="justsubmitted" value="true">
+
+        <?php wp_nonce_field('saveFilterWords', 'ourNonce') ?>
         <label for="plugin_words_to_filter">
             <p><?php esc_html(__('Enter a comma separated list of words to filter from your site\'s content')) ?></p>
             <div class="word-filter__flex-container">
                 <textarea name="plugin_words_to_filter" id="plugin_words_to_filter"
-                    placeholder="bad,mean, awful, horrible"></textarea>
+                    placeholder="bad,mean, awful, horrible">
+                    <?php echo esc_textarea(get_option('plugin_words_to_filter')); ?>
+                </textarea>
             </div>
             <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
         </label>
